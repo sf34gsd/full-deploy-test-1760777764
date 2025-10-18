2243a8c20b5e78fb1333614b41198552967f3f3d
Mykhailo Kolo <mykhailokolo@Mykhailos-Mac-mini.local>
2025-10-18 10:56:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from Cloud Run"}
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
+
+
